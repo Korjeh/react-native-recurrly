d18a4e7a2088c3d4f884a1eabdaa85742ebef93c
Jose <joserecni@gmail.com>
2026-05-12 09:43:18 -0400

    implement fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import "../../global.css"
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import {Link} from "expo-router";
 import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 import { styled } from "nativewind";
@@ -20,9 +20,9 @@ export default function App() {
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
                 Go to Sign Up
             </Link>
-            {/*<Link href="/app/subscriptions/spotify">*/}
-            {/*    Spotify Subscription*/}
-            {/*</Link>*/}
+            <Link href="/subscriptions/spotify">
+                Spotify Subscription
+            </Link>
             <Link
                 href={{
                     pathname: "/subscriptions/[id]",
